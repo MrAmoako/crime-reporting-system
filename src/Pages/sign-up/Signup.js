a66664e56b7cc4db498f7865478271848365af35
Mr-Deee <merchantdaniel8@gmail.com>
2023-07-17 13:06:23 +0000

    police rank

--- src/Pages/sign-up/Signup.js
+++ src/Pages/sign-up/Signup.js
@@ -9,7 +9,6 @@ import "firebase/database";
 const firebaseConfig = {
   // Your Firebase config object here
 
-
   apiKey: "AIzaSyD_ZTkkSPzOrXPA5n_lTcX0U3q16OS-_VU",
   authDomain: "crime-reporting-system-c4a7b.firebaseapp.com",
   projectId: "crime-reporting-system-c4a7b",
@@ -25,6 +24,7 @@ const db = firebase.firestore();
 const SignUpPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [policerank, setpolicerank] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSignup = async (event) => {
@@ -43,7 +43,6 @@ const SignUpPage = () => {
         // });
         // Redirect to the authentication page
 
-
         await db.collection("policeMen").doc(user.id).set({
           name,
           email,
@@ -57,37 +56,70 @@ const SignUpPage = () => {
   };
 
   return (
-    <div>
-      <h1>Police Men Signup</h1>
-      <form onSubmit={handleSignup}>
-        <label>
-          Name:
-          <input
-            type="text"
+    <div className="Auth-form-container">
+      <form className="Auth-form" onSubmit={handleSignup}>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign Up</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={"changeAuthMode"}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
             value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
+              onChange={(e) => setName(e.target.value)}
+              type="name"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group mt-3">
+            <label>Police Rank</label>
+            <input
+            value={policerank}
+              type="Police Rank"
+              className="form-control mt-1"
+              placeholder="Police Rank"
+              onChange={(e) => setpolicerank(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Sign Up</button>
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button
+              type="submit"
+              // onClick={SignUpPage}
+              className="btn btn-primary"
+            >
+              Submit
+            </button>
+          </div>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
       </form>
     </div>
   );
