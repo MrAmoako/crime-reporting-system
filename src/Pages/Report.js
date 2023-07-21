339ddae2701a0843b026da637df0c7f1fce4165e
MrAmoako <simplecraft1212@gmail.com>
2023-07-21 22:25:07 +0000

    cfn4

--- src/Pages/Report.js
+++ src/Pages/Report.js
@@ -1,7 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "./Report.css";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD_ZTkkSPzOrXPA5n_lTcX0U3q16OS-_VU",
+  authDomain: "crime-reporting-system-c4a7b.firebaseapp.com",
+  projectId: "crime-reporting-system-c4a7b",
+  storageBucket: "crime-reporting-system-c4a7b.appspot.com",
+  messagingSenderId: "781871322055",
+  appId: "1:781871322055:web:35597bab711f0f47f0bfe2",
+  measurementId: "G-08E8GHRL5K",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 const Report = () => {
-  return <div>Report</div>;
+  const [reporterName, setReporterName] = useState("");
+  const [reporterPhone, setReporterPhone] = useState("");
+  const [victimName, setVictimName] = useState("");
+  const [victimPhone, setVictimPhone] = useState("");
+  const [suspectName, setSuspectName] = useState("");
+  const [location, setLocation] = useState("");
+  const [dateTime, setDateTime] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Save data to Firebase
+      await db.collection("crimeReports").add({
+        reporterName,
+        reporterPhone,
+        victimName,
+        victimPhone,
+        suspectName,
+        location,
+        dateTime,
+      });
+
+      // Reset form fields
+      setReporterName("");
+      setReporterPhone("");
+      setVictimName("");
+      setVictimPhone("");
+      setSuspectName("");
+      setLocation("");
+      setDateTime("");
+
+      console.log("Data saved successfully!");
+    } catch (error) {
+      console.error("Error saving data:", error);
+    }
+  };
+
+  const handleFetchData = async () => {
+    try {
+      // Fetch data from Firebase
+      const snapshot = await db.collection("crimeReports").get();
+
+      snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <div className="crime-management">
+      <h1>Crime Management System</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Reporter Name:
+          <input
+            type="text"
+            value={reporterName}
+            onChange={(e) => setReporterName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Reporter Phone Number:
+          <input
+            type="tel"
+            value={reporterPhone}
+            onChange={(e) => setReporterPhone(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Victim's Name:
+          <input
+            type="text"
+            value={victimName}
+            onChange={(e) => setVictimName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Victim's Phone Number:
+          <input
+            type="tel"
+            value={victimPhone}
+            onChange={(e) => setVictimPhone(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Suspect's Name:
+          <input
+            type="text"
+            value={suspectName}
+            onChange={(e) => setSuspectName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Location:
+          <input
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Date and Time:
+          <input
+            type="datetime-local"
+            value={dateTime}
+            onChange={(e) => setDateTime(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={handleFetchData}>Fetch Data</button>
+    </div>
+  );
 };
 
 export default Report;
